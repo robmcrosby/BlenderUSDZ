e670a50178cd029f1208874cf4e7586fe1d6a44a
Robert Crosby <robmcrosby@gmail.com>
2021-12-04 22:17:33 -0800

    Fix missing properties for import and export

--- io_scene_usdz/__init__.py
+++ io_scene_usdz/__init__.py
@@ -2,28 +2,39 @@
 bl_info = {
     "name":        "USDZ Export",
     "author":      "Robert Crosby",
-    "version":     (0, 0, 5),
-    "blender":     (2, 80, 0),
+    "version":     (0, 0, 6),
+    "blender":     (3, 0, 0),
     "location":    "File > Import-Export",
     "description": "Import/Export USDZ Files",
     "category":    "Import-Export"
     }
 
+if "bpy" in locals():
+    import importlib
+    if "import_usdz" in locals():
+        importlib.reload(import_usdz)
+    if "export_usdz" in locals():
+        importlib.reload(export_usdz)
 
 import bpy
 from bpy.props import (
-        BoolProperty,
-        FloatProperty,
-        IntProperty,
-        StringProperty,
-        EnumProperty,
-        )
+    BoolProperty,
+    FloatProperty,
+    IntProperty,
+    StringProperty,
+    EnumProperty,
+)
 from bpy_extras.io_utils import (
-        ImportHelper,
-        ExportHelper,
-        path_reference_mode,
-        axis_conversion,
-        )
+    ImportHelper,
+    ExportHelper,
+    path_reference_mode,
+    axis_conversion,
+)
+from bpy.types import (
+    Operator,
+    OperatorFileListElement,
+)
+
 
 class ImportUSDZ(bpy.types.Operator, ImportHelper):
     """Import a USDZ File"""
@@ -34,24 +45,52 @@ class ImportUSDZ(bpy.types.Operator, ImportHelper):
 
     filename_ext = ""
     filter_glob: StringProperty(
-            default="*.usdz;*.usda;*.usdc",
-            options={'HIDDEN'},
-            )
-    materials = BoolProperty(
+        default="*.usdz;*.usda;*.usdc",
+        options={'HIDDEN'},
+    )
+
+    materials: BoolProperty(
         name="Import Materials",
         description="Import Materials and textures",
         default=True,
-        )
-    animations = BoolProperty(
+    )
+    animations: BoolProperty(
         name="Import Animations",
         description="Import Animations",
         default=True,
-        )
+    )
 
     def execute(self, context):
         from . import import_usdz
         keywords = self.as_keywords(ignore=("filter_glob",))
         return import_usdz.import_usdz(context, **keywords)
+
+    def draw(self, context):
+        pass
+
+
+class USDZ_PT_import_main(bpy.types.Panel):
+    bl_space_type = 'FILE_BROWSER'
+    bl_region_type = 'TOOL_PROPS'
+    bl_label = ""
+    bl_parent_id = "FILE_PT_operator"
+
+    @classmethod
+    def poll(cls, context):
+        sfile = context.space_data
+        operator = sfile.active_operator
+        return operator.bl_idname == "IMPORT_OT_usdz"
+
+    def draw(self, context):
+        layout = self.layout
+        layout.use_property_split = True
+        layout.use_property_decorate = False
+
+        sfile = context.space_data
+        operator = sfile.active_operator
+
+        layout.prop(operator, 'materials')
+        layout.prop(operator, 'animations')
 
 
 class ExportUSDZ(bpy.types.Operator, ExportHelper):
@@ -61,56 +100,59 @@ class ExportUSDZ(bpy.types.Operator, ExportHelper):
     bl_label = "Export USDZ File"
     bl_options = {'PRESET'}
 
-    filename_ext = ""
+    filename_ext = ".usdz"
     filter_glob: StringProperty(
-            default="*.usdz;*.usda;*.usdc",
-            options={'HIDDEN'},
-            )
-    exportMaterials = BoolProperty(
+        default="*.usdz;*.usda;*.usdc",
+        options={'HIDDEN'},
+    )
+    exportMaterials: BoolProperty(
         name="Export Materials",
         description="Export Materials from Objects",
         default=True,
-        )
-    exportAnimations = BoolProperty(
+    )
+    exportAnimations: BoolProperty(
         name="Export Animations",
         description="Export Animations",
         default=False,
-        )
-    bakeTextures = BoolProperty(
+    )
+    bakeTextures: BoolProperty(
         name="Bake Textures",
         description="Bake Diffuse, Roughness, Normal, etc",
         default=False,
-        )
-    bakeAO = BoolProperty(
+    )
+    bakeAO: BoolProperty(
         name="Bake AO",
         description="Bake Ambiant Occlusion Texture",
         default=False,
-        )
-    bakeAOSamples = IntProperty(
+    )
+    bakeAOSamples: IntProperty(
         name="AO Samples",
         description="Number of Samples for Ambiant Occlusion",
         min=1,
         max=1000,
         default= 64,
-        )
-    bakeTextureSize = IntProperty(
+    )
+    bakeTextureSize: IntProperty(
         name="Bake Image Size",
         description="Default Size of any Baked Images",
         min=16,
         max=4096,
         default= 1024,
-        )
-    globalScale = FloatProperty(
+    )
+    globalScale: FloatProperty(
         name="Scale",
         min=0.01,
         max=1000.0,
         default=1.0,
-        )
-    useConverter = BoolProperty(
+    )
+    useConverter: BoolProperty(
         name="Use Usdz Converter Tool",
         description="Use Apple's Converter Tool to create the Usdz file",
         default=False,
-        )
+    )
+
+    #path_mode: path_reference_mode
+    #check_extension = True
 
     def execute(self, context):
         from . import export_usdz
@@ -122,9 +164,42 @@ class ExportUSDZ(bpy.types.Operator, ExportHelper):
                                             ))
         return export_usdz.export_usdz(context, **keywords)
 
+    def draw(self, context):
+        pass
+
+
+class USDZ_PT_export_main(bpy.types.Panel):
+    bl_space_type = 'FILE_BROWSER'
+    bl_region_type = 'TOOL_PROPS'
+    bl_label = ""
+    bl_parent_id = "FILE_PT_operator"
+
+    @classmethod
+    def poll(cls, context):
+        sfile = context.space_data
+        operator = sfile.active_operator
+        return operator.bl_idname == "EXPORT_OT_usdz"
+
+    def draw(self, context):
+        layout = self.layout
+        layout.use_property_split = True
+        layout.use_property_decorate = False
+
+        sfile = context.space_data
+        operator = sfile.active_operator
+
+        layout.prop(operator, 'exportMaterials')
+        layout.prop(operator, 'exportAnimations')
+        layout.prop(operator, 'bakeTextures')
+        layout.prop(operator, 'bakeAO')
+        layout.prop(operator, 'bakeAOSamples')
+        layout.prop(operator, 'bakeTextureSize')
+        layout.prop(operator, 'globalScale')
+
 
 def menu_func_usdz_import(self, context):
     self.layout.operator(ImportUSDZ.bl_idname, text="USDZ (.usdz)")
+
 
 def menu_func_usdz_export(self, context):
     self.layout.operator(ExportUSDZ.bl_idname, text="USDZ (.usdz)")
@@ -132,7 +207,9 @@ def menu_func_usdz_export(self, context):
 
 classes = (
     ImportUSDZ,
+    USDZ_PT_import_main,
     ExportUSDZ,
+    USDZ_PT_export_main,
 )
 
 
